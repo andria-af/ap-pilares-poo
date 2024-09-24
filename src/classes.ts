0d912bb0e9a77820c490fc5511dae7af923ebac3
andria-af <andria.af@hotmail.com>
2024-09-24 19:37:28 -0300

    first commit

---
+++ src/classes.ts
@@ -0,0 +1,146 @@
+
+
+//1
+
+export abstract class  Ingresso {
+    constructor(
+        public valorR$: number = 100,
+    ){}
+
+    public imprimeValor(): void {
+
+    }
+}
+
+//a 
+
+export class Normal extends Ingresso{
+    constructor(
+        valorR$ :number,
+    ){super(valorR$)}
+
+    public imprimeValor(): void {
+        console.log(`Valor do ingresso normal R$=${this.valorR$}`) 
+    }
+
+}
+
+//b 
+
+export class Vip extends Ingresso{
+    constructor (
+        public valorAdicional: number = 100,
+        valorR$:number
+    ){super(valorR$)}
+
+    public imprimeValor(): void {
+        this.valorR$= this.valorR$+ this.valorAdicional
+        console.log(`Valor do ingresso Vip = ${this.valorR$}`)
+    }
+}
+
+//c
+
+export class Camarote extends Ingresso{
+    constructor (
+        public valorAdicional: number = 200,
+        valorR$: number
+    ){super(valorR$)}
+
+    public imprimeValor(
+        
+    ): void {
+        this.valorR$= this.valorR$+ this.valorAdicional
+        console.log(`Valor do ingresso Camarote= ${this.valorR$}`)
+    }
+}
+
+//2
+
+export class Imovel {
+    constructor(endereço: string, preco: number){}
+}
+
+//a 
+export class Novo extends Imovel{
+    constructor(endereco: string, preco: number, private _precoAdicional= 1000){
+        super(endereco, preco)
+    }
+
+    public get precoAdicional(): number{
+        return this._precoAdicional
+    }
+
+    public imprimeValor(){
+        console.log(`O valor adicional do imóvel novo é de: R$ ${this._precoAdicional}`)
+    }
+
+}
+
+//a 
+export class Velho extends Imovel{
+    constructor(endereco: string, preco: number, private _precoDesconto= 1000){
+        super(endereco, preco)
+    }
+
+    public get precoDesconto(): number{
+        return this._precoDesconto
+    }
+
+    public imprimeValor(){
+        console.log(`O valor de desconto do imóvel velho é de: - R$ ${this._precoDesconto}`)
+    }
+
+}
+
+//2
+
+export class Animal {
+    constructor(private _especie: string, private _corPelo: string){}
+
+    public come(): void{
+   }
+}
+
+export class Cachorro extends Animal{
+    constructor(_especie: string, _corPelo: string){
+        super(_especie, _corPelo)
+    }
+
+    public late(): void {
+        console.log("Cachorro latindo...")
+    }
+
+    public come(): void {
+        console.log("Comendo...")
+    }
+}
+
+export class Cavalo extends Animal{
+    constructor(_especie: string, _corPelo: string){
+        super(_especie, _corPelo)
+    }
+
+    public relincha(): void {
+        console.log("Cavalo relinchando...")
+    }
+
+    public come(): void {
+        console.log("Comendo...")        
+    }
+}
+
+export class Gato extends Animal{
+    constructor(_especie: string, _corPelo: string){
+        super(_especie, _corPelo)
+    }
+
+    public ronrona(): void {
+        console.log("Gato ronronando...")
+        
+    }
+
+    public come(): void {
+        console.log("Comendo...")
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import { Cavalo, Gato, Cachorro, Ingresso, Normal, Vip, Camarote, Velho, Novo, Imovel} from "./classes";
+
+
+// 1. Crie uma classe abstrata chamada Ingresso que possui um valor
+// em reais e um método imprimeValor()
+// a. crie uma classe Normal, que herda Ingresso..
+// b. crie uma classe VIP, que herda Ingresso e possui um valor
+// adicional. Altere o método imprimeValor para mostrar o valor
+// do ingresso somado com o adicional.
+// c. crie uma classe Camarote, que herda ingresso e possui um
+// valor adicional. Altere o método imprimeValor para mostrar o
+// valor do ingresso somado com o adicional.
+
+const ingresso= new Normal(100);
+ingresso.imprimeValor();
+
+const ingressoVip= new Vip(100, 100);
+ingressoVip.imprimeValor();
+
+const ingressoCamarote= new Camarote(100,200);
+ingressoCamarote.imprimeValor();
+
+// 2
+
+const imovelNovo= new Novo("avenida independencia", 200000);
+imovelNovo.imprimeValor();
+
+const imovelVelho= new Velho("avenida independencia", 200000);
+imovelVelho.imprimeValor();
+
+// 3
+
+const gato= new Gato("Felis catus", "amarelo")
+gato.ronrona();
+
+const cachorro= new Cachorro("C. lupus", "preto")
+cachorro.late();
+
+const cavalo= new Cavalo("Equus ferus", "marrom")
+cavalo.relincha();
